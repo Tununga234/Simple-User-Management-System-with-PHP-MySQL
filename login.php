aec0fd65fbdb5c03a926c83979c95f0ca5b15465
Tununga234 <tunungachakma6@gmail.com>
2025-06-29 19:03:23 +0530

    Update login.php

--- login.php
+++ login.php
@@ -33,11 +33,11 @@ if (isset($logout)) {
 
             <form class="" action="" method="post">
                 <div class="form-group">
-                  <label for="email">Email address</label>
+                  <label for="email">Email address</tunungachakma6@gmail.com
                   <input type="email" name="email"  class="form-control">
                 </div>
                 <div class="form-group">
-                  <label for="password">Password</label>
+                  <label for="password">Password<tununga@123
                   <input type="password" name="password"  class="form-control">
                 </div>
                 <div class="form-group">
